cd8d21f0ad6c99d90ef50b2f453c25366d34bec9
Katya Salas <ksalas@simetrical.com.mx>
2023-01-23 20:58:33 -0600

    update in url for local

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = "https://snte25.org/";
+//$config['base_url'] = "https://snte25.org/";
+$config['base_url'] = 'http://localhost/snte25qroo/';
 
 /*
 |--------------------------------------------------------------------------
