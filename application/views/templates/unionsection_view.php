88eaeeaed301f29a585baaf4a9092034497bf322
Katya Salas <ksalas@simetrical.com.mx>
2023-01-23 20:59:36 -0600

    update code

--- application/views/templates/unionsection_view.php
+++ application/views/templates/unionsection_view.php
@@ -38,7 +38,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+            </div>
+            <div class="row">
+                <div class="col-md-12">
                     <div class="serviceBox" style="margin-top: 100px;">
                         <div class="service-content">
                             <span class="service-icon">
@@ -46,13 +48,328 @@
                             </span>
                             <h1>¿Quienes somos?</h1>
                             <p>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus nisi maximus, pharetra nulla finibus, efficitur mi. Phasellus at ligula quis nunc varius rhoncus. Sed vitae justo at nisl ornare fermentum at sit
-                              amet leo. Donec lobortis elementum faucibus. Vestibulum tempor nec justo in iaculis. Duis tellus mauris, consequat vel dolor sed, volutpat sollicitudin enim. Maecenas velit dolor, efficitur sagittis interdum ac,
-                              pellentesque et velit. Nulla odio erat, pharetra non posuere id, sollicitudin sed erat. Nulla vitae est ligula. Ut aliquam porta orci. Nulla faucibus leo in sapien finibus, vel egestas lorem varius.
-                           </p>
+                                Somos una organización sindical orgullosamente arraigados a una historia de casi ocho décadas al servicio de la educación de Quintana Roo y de México, cuyo objetivo es la defensa y continuo mejoramiento de las condiciones económicas, sociales, laborales y profesionales de sus agremiados.
+                                <br>
+                                Nacimos un día viernes 25 de Agosto del año de 1944 en la ciudad de Mérida Yucatán, en virtud de las extraordinarias privaciones que se vivían en lo que era el territorio federal de Quintana Roo y ello propiciaba que fuera más fácil reunirse y encontrarse en la capital Yucateca en vez de hacerlo en Felipe Carrillo Puerto o ciudad Chetumal.
+                                <br>
+                                Casi todos los maestros vivíamos con nuestras familias incluyendo a los hijos, en nuestras remotas e incomunicadas comunidades por meses y prácticamente sólo podíamos vernos en los períodos vacacionales y era entonces cuando hacíamos reuniones en las que evaluábamos las condiciones que nos tocaba experimentar.
+                                <br>
+                                Carecíamos de salarios dignos, de seguridad en el empleo, de servicios asistenciales y médicos, de arbitrariedades en el trato, en la movilización para tener mejores lugares de adscripción; prácticamente sin condiciones profesionales y de vida, en tanto que además de la labor en las aulas, realizábamos otras actividades de asesoramiento hacia las cooperativas, sindicatos, centrales y organizaciones campesinas, organizábamos a los chicleros, a los madereros, encabezando luchas obreras, enseñando a leer y escribir a los adultos, impulsando una real educación socialista.
+                                <br>
+                                Salarios de noventa pesos mensuales -tres pesos diario- en aquellos tiempos que apenas alcanzaban para sobrevivir; en muchos casos, compañeros que fueron considerados espías del gobierno y que por ello eran hostigados, vivir con las familias en condiciones muy difíciles puesto que eran comunidades que prácticamente carecían de todo además de estar sujetos a cambios de adscripción a donde las autoridades administrativas así lo consideraban, era lo normal de ser maestro. 
+                                <br>
+                                Sin tener conocimiento pleno, problemas similares vivían nuestros compañeros profesores en cada rincón de nuestra patria, quienes también buscaban formas que les permitieran tener una vida en la que desarrollaran a plenitud la labor educativa.
+                            </p>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="row">
+                <div class="container">
+                    <h2 class="pb-3 pt-2 border-bottom mb-5">Un poco más de nuestra historia.</h2>
+                    <p>
+                        A partir del año de 1944 cuando es electo el primer comité ejecutivo seccional que preside el profesor Rodolfo Baeza Sierra a quien acompaña Manuel Escobedo Malaver como secretario de trabajos y conflictos, Ignacio herrera López como secretario de organización, ramón santana Moguel como secretario de finanzas y pedro Presuel Soberanis como secretario de acción social, mismos que asumen un período de representatividad por dos años; hasta el presente año de 2022, se han electo veinticuatro comités ejecutivos seccionales y han sido presididos por:
+                    </p>
+                    <!--first section-->
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Manuel Escobedo Malaver</h5>
+                            <p>(1946 - 1948)</p>
+                        </div>
+                    </div>
+                    <!--path between 1-2-->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <!--second section-->
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Miguel Ortega Navarrete</h5>
+                            <p>(1948 – 1951)</p>
+                        </div>
+                    </div>
+                    <!--path between 2-3-->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <!--third section-->
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Raúl Anguas Rivero</h5>
+                            <p>(1951-1955)</p>
+                        </div>
+                    </div>
+
+                    <!-- Nuevos -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Orlando Ordoñez gallareta</h5>
+                            <p>(1955-1958)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>José España cruz</h5>
+                            <p>(1958-1962)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Eleuterio llanes pasos</h5>
+                            <p>(1962-1965)</p>
+                        </div>
+                    </div>
+
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Isidro Rivera Zetina</h5>
+                            <p>(1965-1968)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Isaías Martínez Martínez</h5>
+                            <p>(1968- 1971)</p>
+                        </div>
+                    </div>
+
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>William Fernández paredes</h5>
+                            <p>(1971-1974)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Orlando Martínez Debeza</h5>
+                            <p>(1974-1977)</p>
+                        </div>
+                    </div>
+
+
+                    <!-- Inicia #1 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Alfredo Díaz Jiménez</h5>
+                            <p>(1977- 1980)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>José Dakac Martínez</h5>
+                            <p>(1980-1983)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #1 -->
+
+                    <!-- Inicia #2 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Raúl Omar santana Bastarrechea</h5>
+                            <p>(1983-1987)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Jorge Milián Narváez</h5>
+                            <p>(1987- 1990)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #2 -->
+
+                    <!-- Inicia #3 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Tito Raúl García Aguilar</h5>
+                            <p>(1990- 1993)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Florentino Balam Xiu</h5>
+                            <p>(1993-1996)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #3 -->
+
+                    <!-- Inicia #4 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Carlos Gutiérrez García</h5>
+                            <p>(1996-2000)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Manuel Tzab Castro</h5>
+                            <p>(2000-2004)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #4 -->
+
+                    <!-- Inicia #5 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Manuel alexander Zetina Aguiluz</h5>
+                            <p>(2004-2008)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Emilio Jiménez Ancona</h5>
+                            <p>(2008-2012)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #5 -->
+
+                    <!-- Inicia #6 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Rafael González Sabido</h5>
+                            <p>(2012-2016)</p>
+                        </div>
+                    </div>
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center justify-content-end how-it-works d-flex">
+                        <div class="col-6 text-right">
+                            <h5>Fermín Pérez Hernández</h5>
+                            <p>(2016-2022)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #6 -->
+
+                    <!-- Inicia #7 -->
+                    <div class="row timeline">
+                        <div class="col-12">
+                            <hr/>
+                        </div>
+                    </div>
+                    <div class="row align-items-center how-it-works d-flex">
+                        <div class="col-6">
+                            <h5>Arimael Salas Alcocer</h5>
+                            <p>(2022- 2026)</p>
+                        </div>
+                    </div>
+                    <!-- Termina #7 -->
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="container">
+                    <p>
+                        Cada uno de ellos con sus respectivos comités, salvo el último que apenas inicia, han impreso su particular sello y los maestros y trabajadores de la educación de sus generaciones tienen un exacto concepto y calificación de sus labores y trayectorias. 
+                        <br>
+                        Como maestros, como sindicalistas, como promotores de la educación que forja generaciones en tiempos y circunstancias, somos orgullosos quintanarroenses de ver nacer de territorio federal a estado, de ser parte de un real crecimiento político social de la entidad, de seguir en la búsqueda de mejorar aún más, las condiciones sociales a través de la educación y de un compromiso con nuestro entorno.
+                        <br>
+                        Reconocemos también que hemos pasado por momentos muy difíciles como cuando ante la represión y colusión de autoridades educativas con los representantes sindicales en el año de 1970 los maestros se insubordinan y reivindican la dignidad prácticamente desconociendo a la dirigencia y eligiendo en el cambio del comité ejecutivo seccional, al profesor William Fernández paredes, quien con otros compañeros habían encabezado tal rebelión; cuando también los maestros se movilizan para hacer valer sus derechos ante la delegada estatal de la SEP en el estado, alma Lourdes Ruíz, quién al final fue removida del cargo al no poder tener buenos acuerdos con los trabajadores y sus dirigentes en esos momentos, el movimiento magisterial en el año de 2013, cuando el gobierno estatal avaló forzosas disposiciones lesivas que por supuesto fueron rechazadas y con marchas, manifestaciones y plantones reprobaron tales medidas, estos casos y otros han sido parte histórica de la combatiente sección 25.
+                        <br>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <h5 class="section-title">NUESTRO COMITE</h5>
+                <img style="width: 100%;" src="<?php echo base_url('assets/images/comite/1.png'); ?>" />
+                <img style="width: 100%;" src="<?php echo base_url('assets/images/comite/2.png'); ?>" />
+                <img style="width: 100%;" src="<?php echo base_url('assets/images/comite/3.png'); ?>" />
+                <img style="width: 100%;" src="<?php echo base_url('assets/images/comite/4.png'); ?>" />
             </div>
         </div>
     </div>
